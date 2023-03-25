4cba8a45e5a41417648c2747306d783a3db03d76
Caio Dias <93464556+caiorodias@users.noreply.github.com>
2023-03-24 23:18:03 -0300

    atualizando

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200) #maximo 200 caracteres
+    brand = models.CharField(max_length=200) #marca
+    factory_year = models.IntegerField(blank=True, null=True) #ano #o blank e o null como true, quer dizer que se quiser da pra deixar em branco, nesse caso só seriam obrigatorios o modelo e a marca.
+    model_year = models.IntegerField(blank=True, null=True) #ano
+    value = models.FloatField(blank=True, null=True) #ponto flutuante
+    
+    
