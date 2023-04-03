5353fcd76077b59785a4124e7d6ba2a5f506ef9c
caiorodias <caiorodias@hotmail.com>
2023-04-03 20:56:05 -0300

    Introduzindo template

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,9 @@
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def cars_view(request):
-    return HttpResponse('Meus Carros')
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': {'model': 'Astra 2.0'}} #context
+        ) #Renderiza uma requisição http e transforma numa response http
