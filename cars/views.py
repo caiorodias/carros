13807483b91de90316619f360a35210de8ab8537
caiorodias <caiorodias@hotmail.com>
2023-04-08 20:44:53 -0300

    Trazendo objetos da tabela para o html

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all() # Igual SELECT * FROM CARS ( TRAZ TODOS OS DADOS DO BANCO DE DADOS)
+    
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'}} #context
-        ) #Renderiza uma requisição http e transforma numa response http
+        {'cars': cars} #context
+    ) #Renderiza uma requisição http e transforma numa response http
