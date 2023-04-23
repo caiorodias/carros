3d1badb627daf19b69882f928e35a3a19f427acf
caiorodias <caiorodias@hotmail.com>
2023-04-23 12:57:40 -0300

    Adicionando novos campos no cadastro do usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm # Form já pronto para crianção de user
+from accounts.forms import CustomUserCreationForm # Form já pronto para crianção de user
 from django.shortcuts import render, redirect
 
 def register_view(request):
     if request.method == "POST":
-        user_form = UserCreationForm(request.POST)
+        user_form = CustomUserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
             return redirect('cars_list')
     else:
-        user_form = UserCreationForm()
+        user_form = CustomUserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
