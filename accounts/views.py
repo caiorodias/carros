bcc293a3034b4f9168192f1b29343564ea192b45
caiorodias <caiorodias@hotmail.com>
2023-04-24 21:25:35 -0300

    criando abas de login e logout e ajustando menu

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,33 @@
-from accounts.forms import CustomUserCreationForm # Form já pronto para crianção de user
+from django.contrib.auth.forms import AuthenticationForm # AutenticationForm vai pedir usuario e senha
+from django.contrib.auth import authenticate, login, logout
+from accounts.forms import CustomUserCreationForm # Form já pronto para crianção de user 
 from django.shortcuts import render, redirect
 
 def register_view(request):
     if request.method == "POST":
-        user_form = CustomUserCreationForm(request.POST)
+        user_form = CustomUserCreationForm(request.POST) # Esses if é só para criar validação
         if user_form.is_valid():
             user_form.save()
-            return redirect('cars_list')
+            return redirect('login')
     else:
         user_form = CustomUserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password) # Autenticação do usuário
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list') # Se for validado ele loga e vai pra a lista de carros
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form':login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view #para puxar a função que está em views
-from accounts.views import register_view
+from accounts.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name = 'register'), #add
+    path('login/', login_view, name= 'login'),
+    path('logout/', logout_view, name='logout'),
     path('cars/', cars_view, name= 'cars_list'), # Dando um nome para a url #add
     path('new_car/', new_car_view, name = 'new_car'), #add
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
